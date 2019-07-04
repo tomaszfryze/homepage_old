df20ce6ee8cb740fde3131f7b9d134b5046fcfde
Tomasz Fryze <t.fryze@gmail.com>
2019-07-04 21:48:34 +0200

    BEM, DOM

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('Elo elo 5 2 0!');
+
+const main__header = document.querySelector('.main__header');
+
+console.log(main__header.innerHTML);
+
+main__header.innerHTML = 'Pozdro!';
+
+const day = document.querySelectorAll('.to-do__day--js');
+
+console.log(day);
+console.log(day[4].innerHTML);
