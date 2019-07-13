0e02aeb27d816c126852ec1cc24fc4019864fa40
Tomasz Fryze <t.fryze@gmail.com>
2019-07-13 19:11:25 +0200

    feat: week5

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log('Elo elo 5 2 0!');
 
-const main__header = document.querySelector('.main__header');
+const main__header = document.querySelector('.header__title');
 
 console.log(main__header.innerHTML);
 
@@ -10,3 +10,17 @@ const day = document.querySelectorAll('.to-do__day--js');
 
 console.log(day);
 console.log(day[4].innerHTML);
+
+
+const age = 27;
+const name = 'Tomek';
+
+function welcome(name, age) {
+    console.log(`Cześć, mam na imię ${name} i mam ${age} lat`);
+}
+welcome("Tomasz", 27);
+
+const welcome1 = (name, age) => {
+    console.log(`Cześć, mam na imię ${name} i mam ${age} lat`);
+}
+welcome1(name, age);
